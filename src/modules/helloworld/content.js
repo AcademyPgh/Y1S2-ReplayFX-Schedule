b1f1d659a347663f2e440820c0afb2b27cd942bc
Cray J Thomas Jr <cjthoma1@gmail.com>
2016-11-30 15:47:26 -0500

    display more button works

--- src/modules/helloworld/content.js
+++ src/modules/helloworld/content.js
@@ -3,39 +3,60 @@ import {
   Text,
   View,
   ListView,
-  Button,
   Alert,
-  TouchableHighlight
+  TouchableHighlight,
+  Modal
 } from 'react-native';
+import autobind from 'class-autobind';
 import styles from './StyleSheet';
 
 import content_sections from './content_sections';
 
-const onButtonPress = () => {
-  Alert.alert('Button has been pressed!');
-};
-
 export default class Content extends Component {
   constructor(props) {
     super(props);
+
     const ds = new ListView.DataSource({
       rowHasChanged: (r1, r2) => r1 !== r2,
       sectionHeaderHasChanged: (s1, s2) => s1 !== s2
     });
     this.state = {
       dataSource: ds.cloneWithRowsAndSections(content_sections(this.props.typeIs)),
-      someText: 'App Made By Academy Pittsburgh'
+      someText: 'App Made By Academy Pittsburgh',
+      modalVisible: false,
+      modalTitle: '',
+      modalDescription: ''
     };
+    this.renderScheduleItem = this.renderScheduleItem.bind(this);
+    this.setModalVisible = this.setModalVisible.bind(this);
   }
+  onButtonPress() {
+    //This is where we will display "more description"
+    // this.setState({modalVisible: true});
+    Alert.alert('hi');
 
+  }
+  setModalVisible(visible, title, description) {
+    this.setState({modalVisible: visible, modalTitle: title, modalDescription: description});
+
+  }
   renderScheduleItem(item) {
-    return (<TouchableHighlight underlayColor='#000000'>
+    return (
       <View>
         <Text style={styles.title}>{item.title}</Text>
         <Text style={styles.datetime}>@ {item.starttime} - {item.endtime}</Text>
         <Text style={styles.description}>{item.description}</Text>
+        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+
+        <TouchableHighlight onPress={() => {
+          this.setModalVisible(true, item.title, item.description);
+        }}>
+          <Text>[+]</Text>
+        </TouchableHighlight>
+
+       </View>
       </View>
-    </TouchableHighlight>);
+    );
 
   }
 
@@ -48,7 +69,26 @@ export default class Content extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <TouchableHighlight onPress={() => {
+          this.setModalVisible(!this.state.modalVisible);
+        }}>
         <Text>{this.state.someText}</Text>
+      </TouchableHighlight>
+        <Modal
+          animationType={'fade'}
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {Alert.alert('Modal has been closed!');}}>
+          <View>
+            <Text>{this.state.modalTitle}</Text>
+            <Text>{this.state.modalDescription}</Text>
+            <TouchableHighlight onPress={() => {
+              this.setModalVisible(!this.state.modalVisible);
+            }}>
+              <Text>hide</Text>
+            </TouchableHighlight>
+          </View>
+        </Modal>
         <ListView
           dataSource={this.state.dataSource}
           renderRow={this.renderScheduleItem}
