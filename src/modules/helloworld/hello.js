b0da3105ac74993ad76dd9552842b656427b4bb2
Cray J Thomas Jr <cjthoma1@gmail.com>
2016-12-08 13:38:47 -0500

    Android Back Button

--- src/modules/helloworld/hello.js
+++ src/modules/helloworld/hello.js
@@ -10,6 +10,16 @@ import {
 
 //Blink is our main component that renders all of our other components
 export default class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.backButton = this.backButton.bind(this);
+    this.backButton();
+  }
+  backButton() {
+    BackAndroid.addEventListener('hardwareBackPress', () => {
+      BackAndroid.exitApp(0);
+    });
+  }
   render() {
     return (
       // Container that just centers our logo
@@ -23,7 +33,7 @@ export default class Blink extends Component {
         {/* Renders the swipeable nav bar */}
         <Scroll_Tab_View/>
             {/* Shows our "main content" */}
-            {BackAndroid.exitApp(0)}
+
             </View>
     );
   }
